4ca297c549cb4a55f0f9c9753110aa907cc6adb0
Emanuel Mendoza Mori <130607153+EmanuelMdz@users.noreply.github.com>
2023-05-17 20:23:28 -0300

    Camibio de tuitulo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = (props) => {
   return (
     <div className={`div_header ${props.theme}`}>
-      <h1>TODO</h1>
+      <h1>TODO change</h1>
       <div className="boton_day_night">
         <button
           onClick={() => props.setTheme(props.theme === "sun" ? "moon" : "sun")}
